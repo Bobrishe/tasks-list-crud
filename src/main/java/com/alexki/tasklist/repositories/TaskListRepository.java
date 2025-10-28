ec5100cf45a2b85c711fcdf970f84e84ae116cdd
beaver <alex101ki@gmail.com>
2025-10-28 12:02:01 +0200

    Made repositories + docker-compose + configurations

---
+++ src/main/java/com/alexki/tasklist/repositories/TaskListRepository.java
@@ -0,0 +1,12 @@
+package com.alexki.tasklist.repositories;
+
+import com.alexki.tasklist.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+
+}
